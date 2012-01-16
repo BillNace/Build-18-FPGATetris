9d03713bbc7e7bd57b61d97869323f4492947b5d
Bill Nace <wnace@cmu.edu>
2012-01-15 19:52:43 -0500

    Added tetris.sv

---
+++ tetris.sv
@@ -0,0 +1,14 @@
+module tetris (
+  output logic [7:0]  tetris_red,
+  output logic [7:0]  tetris_green, 
+  output logic [7:0]  tetris_blue, 
+  input  logic [10:0] col, 
+  input  logic [10:0] row, 
+  input  logic        CLOCK_50,
+  input  logic [3:0]  KEY);
+  
+  assign tetris_red   = row[10:3];
+  assign tetris_green = row[10:3];
+  assign tetris_blue  = col[10:3];
+  
+endmodule: tetris
